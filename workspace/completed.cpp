9c63cf75b27f6a82e880d99b2baa5080f912150d
Eric <schmackelnuts+ghschool@gmail.com>
2015-02-19 07:51:26 +0000

    added completed class

---
+++ workspace/completed.cpp
@@ -0,0 +1,36 @@
+#include "Date.h"
+#include "Assignment.h"
+#include "Node.h"
+#include "linked_list.cpp"
+#include <string>
+using namespace std;
+
+class completed {
+  public:
+      completed ();
+      void display ();
+      void add ();
+      void save ();
+  private:
+      void order ();  
+};
+
+completed :: completed (){
+  
+}
+
+void completed :: display (){
+  
+}
+
+void completed :: add (){
+  
+}
+
+void completed :: save (){
+  
+}
+
+void completed :: order (){
+  
+}
