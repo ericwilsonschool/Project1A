48eadc9ab3ec140fbdc1ce2c353176974b9e947d
Eric <schmackelnuts+ghschool@gmail.com>
2015-02-19 07:50:47 +0000

    added linked list class

---
+++ workspace/linked_list.cpp
@@ -0,0 +1,37 @@
+#include 'Node.h'
+#include <string>
+using namespace std;
+
+template <typename item_type>
+class linked_list{
+  public:
+  linked_list ();
+  linked_list (item_type data);
+  ~linked_list ();
+  void add_node (item_type data);
+  void delete_node (item_type data);
+  
+  private:
+  Node* head;
+  Node* tail;
+};
+
+linked_list :: linked_list (){
+  
+}
+
+linked_list :: linked_list (item_type data){
+  
+}
+
+linked_list :: ~linked_list (){
+  
+}
+
+void linked_list :: add_node (item_type data){
+  
+}
+
+void linked_list :: delete_node (item_type data){
+  
+}
