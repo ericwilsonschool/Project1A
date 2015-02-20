7a7e2655271ab25b2d352d5c835a07cd39c45631
John Buckley <jbuckley5@outlook.com>
2015-02-20 03:29:15 -0600

    Fixed whitespace

--- workspace/date.cpp
+++ workspace/date.cpp
@@ -1,40 +1,36 @@
-//Just a stupid dream... :(
-//Eric Wilson
+// Just a stupid dream... :(
+// Eric Wilson
 #include <iostream>
 using namespace std;
 
-class Date{
-  private:
-    int month;
-    int day;
-    int year;
-  public:
-    Date(int , int, int);
-    Date();
-    void print();
-    void change(int , int , int );
-  
+class Date {
+private:
+  int month;
+  int day;
+  int year;
+
+public:
+  Date(int, int, int);
+  Date();
+  void print();
+  void change(int, int, int);
 };
 
-Date::Date(int m, int d, int y){
+Date::Date(int m, int d, int y) {
   month = m;
   day = d;
   year = y;
 }
-  
-Date::Date(){
+
+Date::Date() {
   month = 0;
   day = 0;
   year = 0;
 }
 
+void Date::print() { cout << month << '-' << day << '-' << year; }
 
-
-void Date::print(){
-  cout << month << '-' << day << '-' << year;
-}
-
-void Date::change(int m, int d, int y){
+void Date::change(int m, int d, int y) {
   month = m;
   day = d;
   year = y;
--- workspace/main.cpp
+++ workspace/main.cpp
@@ -1,27 +1,28 @@
-//How do I stop this crazy thing?
-//Eric Wilson
+// How do I stop this crazy thing?
+// Eric Wilson
 
-//Oh, and apparently there's an option under the 'Edit' menu to use Vim key bindings. 
+// Oh, and apparently there's an option under the 'Edit' menu to use Vim key
+// bindings.
 #include <iostream>
 #include "date.cpp"
 #include "menu.cpp"
 
 using namespace std;
 
-int main()
-{ 
- Date newDate (11,19,1987); //test constructor with my birfday
- Date newDate2;//test default constructor
-  
+int main() {
+  Date newDate(11, 19, 1987); // test constructor with my birfday
+  Date newDate2; // test default constructor
+
   newDate.print();
   cout << '\n';
   newDate2.print();
   cout << '\n';
-  newDate2.change(11, 13, 1989); //test change function with Brittney's birfday.
+  newDate2.change(11, 13, 1989); // test change function with Brittney's
+                                 // birfday.
   newDate2.print();
   cout << '\n';
-  
-  mainMenu();//start menu navigation (menu.cpp)
+
+  mainMenu(); // start menu navigation (menu.cpp)
 
   return 0;
 }
