dcfad9e87f744fed3a81c684c834562ed1792c67
John Buckley <jbuckley5@outlook.com>
2015-02-21 00:15:47 -0600

    Add constructor to struct, populated test.txt

--- workspace/Assignment.h
+++ workspace/Assignment.h
@@ -1,16 +1,15 @@
-#ifndef ASSIGNMENT_H
-#define ASSIGNMENT_H
-
 #include "Date.h"
 #include <string>
 using namespace std;
 
 struct Assignment{
+    Date assigned;
     Date due;
     Date completed;
-    Date assigned;
     string description;
     bool isLate;
+    
+    Assignment() : assigned(1,1,2015), due(12,30,2015), completed(8,17,2015), description("Empty constructor"), isLate(false)
+    Assignment(Date p_assigned, Date p_due, Date p_completed, string p_description, bool p_isLate) : assigned(p_assigned), due(p_due), completed(p_completed), description(p_description), isLate(p_isLate)
 };
 
-#endif
